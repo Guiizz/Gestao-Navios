1537fa0487dcee7456ab046e94f006ab4a6645cf
1251652 <1251652@isep.ipp.pt>
2026-06-19 19:22:53 +0100

    correcao para a vista ficar a funcionar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,15 @@ module com.gestaonavios.gestaonavios {
     requires javafx.fxml;
     requires java.sql;
 
-
     opens com.gestaonavios.gestaonavios to javafx.fxml;
+    opens com.gestaonavios.gestaonavios.View to javafx.fxml;       // ADICIONADO
+    opens com.gestaonavios.gestaonavios.Controller to javafx.fxml; // ADICIONADO
+    opens com.gestaonavios.gestaonavios.Model to javafx.fxml;      // ADICIONADO
+    opens com.gestaonavios.gestaonavios.BLL to javafx.fxml;        // ADICIONADO
+    opens com.gestaonavios.gestaonavios.DAL to javafx.fxml;        // ADICIONADO
+
     exports com.gestaonavios.gestaonavios;
+    exports com.gestaonavios.gestaonavios.View;       // ADICIONADO
+    exports com.gestaonavios.gestaonavios.Controller; // ADICIONADO
+    exports com.gestaonavios.gestaonavios.Model;      // ADICIONADO
 }
